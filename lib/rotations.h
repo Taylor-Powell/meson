54de2564f7e53c9db2295d979c1507ebab786446
Taylor-Powell <trpowell1994@gmail.com>
2025-02-18 13:48:38 -0500

    Also made the rotPolVec_Init() function work from a loopup table

--- lib/rotations.h
+++ lib/rotations.h
@@ -21,16 +21,10 @@ namespace {
     double twopi = 2.0 * std::numbers::pi;
 }
 namespace rotations {
-    void rotPolVec_init(vec2D<cd>& polVec, std::string sym) {
-        if (sym == "Dic4") return; // no rotation needed  
-        else if (sym == "Dic2") polVec = basics::rotVec(polVec, pi2, pi4, -pi2);
-        else if (sym == "Dic3") polVec = basics::rotVec(polVec, pi4, 0.955316618124509, 0); // acos(1/sqrt[3])
-        else if (sym == "C40mn") polVec = basics::rotVec(polVec, pi2, 0.4636476090008061, 0); // acos(2/sqrt[5])
-        else if (sym == "C4nnm") polVec = basics::rotVec(polVec, -3.0 * pi4, 0.6154797086703874, 0); // acos(2/sqrt[6])
-        else throw std::string("Symmetry " + sym + " not recognized in rotations::rotPolVec_init.\n");
-    }
-
     const std::vector<std::string> symList = {"Dic4", "Dic2", "Dic3", "C40mn", "C4nnm"};
+    const std::vector<std::vector<double>> symInit_angles = {
+        {0, 0, 0}, {0, pi2, 0}, {0, pi4, 0.955316618124509}, {0, pi2, 0.4636476090008061}, {0, -3.0 * pi4, 0.6154797086703874}
+    };
     const std::vector<std::vector<std::vector<int>>> sym_moms = {
         {{0, 0, 1}, {1, 0, 0}, {0, 1, 0}, {-1, 0, 0}, {0, -1, 0}, {0, 0, -1}},    
         {{0, 1, 1}, {1, 1, 0}, {1, 0, 1}, {1, -1, 0}, {0, 1, -1}, {-1, 0, 1}, {-1, 1, 0}, {0, -1, 1}, {1, 0, -1}, {-1, -1, 0}, {0, -1, -1}, {-1, 0, -1}},
@@ -45,6 +39,15 @@ namespace rotations {
         {{0, 0, 0}, {3.0 * pi2, -pi2, 0}, {0, pi2, pi2}, {3.0 * pi2, -pi2, 3.0 * pi2}, {0, pi2, 0}, {0, 0, 3.0 * pi2}, {0, -pi, 0}, {3.0 * pi2, pi2, 0}, {0, -pi2, 3.0 * pi2}, {3.0 * pi2, pi2, pi2}, {0, -pi2, 0}, {0, -pi, pi2}, {0, 0, pi}, {3.0 * pi2, -pi2, pi}, {0, pi2, 3.0 * pi2}, {3.0 * pi2, -pi2, pi2}, {0, pi2, pi}, {0, 0, pi2}, {0, -pi, pi}, {3.0 * pi2, pi2, pi}, {0, -pi2, pi2}, {3.0 * pi2, pi2, 3.0 * pi2}, {0, -pi2, pi}, {0, -pi, 3.0 * pi2}},
         {{0, 0, 0}, {3.0 * pi2, -pi2, 0}, {0, pi2, pi2}, {0, 0, pi2}, {3.0 * pi2, -pi2, 3.0 * pi2}, {0, 0, 3.0 * pi2}, {3.0 * pi2, -pi2, pi2}, {0, pi2, pi}, {0, pi2, 0}, {0, -pi, pi2}, {3.0 * pi2, pi2, pi2}, {0, -pi2, 0}, {0, 0, pi}, {3.0 * pi2, -pi2, pi}, {0, pi2, 3.0 * pi2}, {0, -pi, 0}, {3.0 * pi2, pi2, pi}, {0, -pi, pi}, {3.0 * pi2, pi2, 0}, {0, -pi2, 3.0 * pi2}, {0, -pi2, pi2}, {0, -pi, 3.0 * pi2}, {3.0 * pi2, pi2, 3.0 * pi2}, {0, -pi2, pi}}
     };
+
+    void rotPolVec_init(vec2D<cd>& polVec, std::string sym) {
+        for (int i = 0; i < symList.size(); i++) {
+            if (symList[i] == sym) {
+                polVec = basics::rotVec(polVec, symInit_angles[i][0], symInit_angles[i][1], symInit_angles[i][2]);
+                return;
+            }
+        }
+    }
 
     void rotPolVec(vec2D<cd>& polVec, std::string sym, std::vector<int> mom) {
         for (int i = 0; i < symList.size(); i++) {
