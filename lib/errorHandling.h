424b98a722bede312c20615eeab3739c698299ba
Taylor-Powell <trpowell1994@gmail.com>
2025-04-03 10:39:47 -0400

    Brought errorHandling in line with version from C++ Standard Library

--- lib/errorHandling.h
+++ lib/errorHandling.h
@@ -3,17 +3,35 @@
 
 #include <iostream>
 #include <stdexcept>
+#include <future>
+#include <system_error>
+#include <exception>
 #include <string>
 
 namespace error {
-
     /**
      * Processes exceptions and outputs appropriate error messages.
      * This function should be called inside a catch block.
+     * Pulled from The C++ Standard Library 2nd edition (page 50-51)
      */
     inline void processException() {
         try {
             throw; // Re-throw the current exception to identify its type
+        }
+        catch (const std::ios_base::failure& e) {
+            std::cerr << "\n\nI/O EXCEPTION:\n" << e.what() << std::endl;
+            processCodeException(e);
+        }
+        catch (const std::system_error& e) {
+            std::cerr << "\n\nSystem EXCEPTION:\n" << e.what() << std::endl;
+            processCodeException(e);
+        }
+        catch (const std::future_error& e) {
+            std::cerr << "\n\nFuture EXCEPTION:\n" << e.what() << std::endl;
+            processCodeException(e);
+        }
+        catch (const std::bad_alloc& e) {
+            std::cerr << "\n\nBAD ALLOC EXCEPTION:\n" << e.what() << std::endl;
         }
         catch (const std::invalid_argument& e) {
             std::cerr << "\n\nInvalid Argument Error:\n" << e.what() << std::endl;
@@ -35,6 +53,16 @@ namespace error {
         }
     }
 
+    template <typename T>
+    void processCodeException(const T& e) {
+        auto c = e.code();
+        std::cerr << "- category: " << c.category().name() << std::endl;
+        std::cerr << "-value: " << c.value() << std::endl;
+        std::cerr << "- message: " << c.message() << std::endl;
+        std::cerr << "- def category: " << c.default_error_condition().name() << std::endl;
+        std::cerr << "- def value: " << c.default_error_condition().value() << std::endl;
+        std::cerr << "- def message: " << c.default_error_condition().message() << std::endl;
+    }
 }
 
 #endif
